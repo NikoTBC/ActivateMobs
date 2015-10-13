879b1e96127c69c50dc7f96d2d07363bf1ced5c8
NikoTBC <niko2002ultimategamer@gmail.com>
2015-10-13 23:35:26 +0800

    No AI, spawn with spawn eggs only

---
+++ src/ActivateMobs/Entities/Pig.php
@@ -0,0 +1,46 @@
+<?php
+
+namespace ActivateMobs\Entities;
+
+use pocketmine\Player;
+use pocketmine\network\protocol\AddEntityPacket;
+use pocketmine\network\AxisAllignedBB;
+use pocketmine\item\Item;
+
+class Chicken extends Animals{
+  const NETWORK_ID = 12;
+
+public function __construct($chunk, $nbt){
+  parent::__construct($chunk, $nbt){
+
+  $this->width = 0.3;
+  $this->length = 0.9;
+  $this->height = 1.9;
+}
+
+public function getName(){
+  return "Pig";
+}
+
+public function spawnTo(Player, $player){
+
+  $pk = new AddEntityPacket();
+  $pk->eid = $this->getId();
+  $pk->type = self::NETWORK_ID;
+  $pk->x = $this->x;
+  $pk->y = $this->y;
+  $pk->z = $this->z;
+  $pk->speedX = $this->motionX;
+  $pk->speedY = $this->motionY;
+  $pk->speedZ = $this->motionZ;
+  $pk->yaw = $this->yaw;
+  $pk->pitch = $this->pitch;
+  $pk->metadata = $this->dataProperties;
+  $player->dataPacket($pk);
+
+  parent::spawnTo($player);
+  }
+ }
+}
+
+?>
