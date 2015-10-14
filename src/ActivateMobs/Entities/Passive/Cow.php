b6e13e2e8b694da68a853751ab7b24e7b003ddd9
NikoTBC <niko2002ultimategamer@gmail.com>
2015-10-14 15:37:33 +0800

    No AI, spawn with spawn eggs only

---
+++ src/ActivateMobs/Entities/Passive/Cow.php
@@ -0,0 +1,47 @@
+<?php
+
+namespace ActivateMobs\Entities\Passive;
+
+use pocketmine\network\Network;
+use pocketmine\network\protocol\AddEntityPacket;
+use pocketmine\Player;
+use pocketmine\entity\Animal;
+use pocketmine\event\entity\EntityDamageByEntityEvent;
+use pocketmine\item\Item as ItemItem;
+
+class Cow extends Animals{
+  const NETWORK_ID = 11;
+
+public function __construct($chunk, $nbt){
+  parent::__construct($chunk, $nbt){
+
+  $this->width = 1;
+  $this->height = 1;
+}
+
+public function getName(){
+  return "Cow";
+}
+
+public function spawnTo(Player, $player){
+
+  $pk = new AddEntityPacket();
+  $pk->eid = $this->getId();
+  $pk->type = self::NETWORK_ID;
+  $pk->x = $this->x;
+  $pk->y = $this->y;
+  $pk->z = $this->z;
+  $pk->speedX = $this->motionX;
+  $pk->speedY = $this->motionY;
+  $pk->speedZ = $this->motionZ;
+  $pk->yaw = $this->yaw;
+  $pk->pitch = $this->pitch;
+  $pk->metadata = $this->dataProperties;
+  $player->dataPacket($pk);
+
+  parent::spawnTo($player);
+  }
+ }
+}
+
+?>
