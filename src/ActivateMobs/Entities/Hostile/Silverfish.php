5ef4257abcd3fd4fdc1996d7206b4ebf705cfc6b
NikoTBC <niko2002ultimategamer@gmail.com>
2015-10-14 18:04:51 +0800

    No AI, spawn with spawn eggs only

---
+++ src/ActivateMobs/Entities/Hostile/Silverfish.php
@@ -0,0 +1,48 @@
+<?php
+
+namespace ActivateMobs\Entities\Hostile;
+
+use pocketmine\network\Network;
+use pocketmine\network\AddEntityPacket;
+use pocketmine\event\entity\EntityDamageByEntityEvent;
+use pocketmine\entity\Monster;
+use pocketmine\Player;
+use pocketmine\item\Item as ItemItem;
+
+class Silverfish extends Monster{
+  const NETWORK_ID = 39;
+
+public function __construct($chunk, $nbt){
+  parent::__construct($chunk, $nbt){
+
+  $this->width = 0.6;
+  $this->length = 0.6;
+  $this->height = 1.8;
+}
+
+public function getName(){
+  return "Silverfish";
+}
+
+public function spawnTo(Player, $player){
+
+  $pk = new AddEntityPacket();
+  $pk->eid = $this->getId();
+  $pk->type = self::NETWORK_ID;
+  $pk->x = $this->x;
+  $pk->y = $this->y;
+  $pk->z = $this->z;
+  $pk->speedX = $this->motionX;
+  $pk->speedY = $this->motionY;
+  $pk->speedZ = $this->motionZ;
+  $pk->yaw = $this->yaw;
+  $pk->pitch = $this->pitch;
+  $pk->metadata = $this->dataProperties;
+  $player->dataPacket($pk);
+
+  parent::spawnTo($player);
+  }
+ }
+}
+
+?>
